1f2baea6c1d9566db5d204ccbaab6a0ffa904f1c
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-02-23 14:44:32 +0100

    configurated templates and views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request=request, template_name='index.html')
+
+
+def products(request):
+    return render(request=request, template_name='products.html')
